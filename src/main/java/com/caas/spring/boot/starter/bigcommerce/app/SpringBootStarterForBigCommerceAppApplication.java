4a661651b8ab66f4db049ec5ee031de5db1fa754
Yoann Esnaud <yoann_esnaud@hotmail.com>
2021-02-18 20:12:51 +0000

    initial commit

---
+++ src/main/java/com/caas/spring/boot/starter/bigcommerce/app/SpringBootStarterForBigCommerceAppApplication.java
@@ -0,0 +1,13 @@
+package com.caas.spring.boot.starter.bigcommerce.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterForBigCommerceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStarterForBigCommerceAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/caas/spring/boot/starter/bigcommerce/app/SpringBootStarterForBigCommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caas.spring.boot.starter.bigcommerce.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterForBigCommerceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
